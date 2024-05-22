39884c5d653a770db09fab4358e669735907c87f
Akash <akashkashyaps123@gmail.com>
2024-05-22 12:16:23 +0100

    added data generator and modified the links and vectorstore

---
+++ RAG_Phi3.py
@@ -0,0 +1,119 @@
+# Import dependancies
+import torch
+import transformers
+import pandas as pd
+import numpy as np
+from torch import cuda, bfloat16
+from langchain.llms import HuggingFacePipeline
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain.chains import RetrievalQA
+from transformers import StoppingCriteriaList, StoppingCriteria
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from langchain.chains.question_answering import load_qa_chain
+from langchain import HuggingFaceHub
+from langchain.prompts import PromptTemplate
+
+# Define the model
+model_id = "microsoft/Phi-3-mini-4k-instruct"
+hf_auth = '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'
+tokenizer = transformers.AutoTokenizer.from_pretrained(
+    model_id,
+    use_auth_token=hf_auth,
+    trust_remote_code=True
+)
+device = f'cuda:{cuda.current_device()}' if cuda.is_available() else 'cpu'
+
+# Configure BitsAndBytesConfig for quantization
+bnb_config = transformers.BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type='nf4',
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=bfloat16
+)
+
+model_config = transformers.AutoConfig.from_pretrained(
+    model_id,
+    use_auth_token=hf_auth,
+    trust_remote_code=True
+)
+model = transformers.AutoModelForCausalLM.from_pretrained(
+    model_id,
+    trust_remote_code=True,
+    config=model_config,
+    quantization_config=bnb_config,
+    device_map='auto',
+    use_auth_token=hf_auth,
+)   
+
+# Set the model to evaluation mode
+model.eval()
+
+# Define the text generation pipeline
+generate_text = transformers.pipeline(
+    model=model,
+    tokenizer=tokenizer,
+    return_full_text=True,
+    task='text-generation',
+    max_new_tokens=512,
+    do_sample=True,
+    temperature=0.1,
+    repetition_penalty=1.1
+)
+
+# Test the model with a prompt
+input_text = "what features does nottingham trent university computer science department have?"
+generated_ids = generate_text(input_text)[0]['generated_text']
+print(generated_ids)
+
+
+from langchain.document_loaders import SeleniumURLLoader
+
+urls = ["https://www.ntu.ac.uk/course/computer-science", 
+        "https://www.ntu.ac.uk/study-and-courses/courses/our-facilities/computer-science-facilities"]
+       
+loader = SeleniumURLLoader(urls)
+documents = loader.load()
+
+# Split the documents into chunks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+all_splits = text_splitter.split_documents(documents)
+
+# Define the model name and kwargs for embeddings
+model_name = "sentence-transformers/all-mpnet-base-v2"
+model_kwargs = {"device": "cuda"}
+
+# Create the HuggingFaceEmbeddings
+embeddings = HuggingFaceEmbeddings(model_name=model_name, model_kwargs=model_kwargs)
+
+# Create the vectorstore
+vectorstore = Chroma.from_documents(
+    all_splits,
+    embeddings,
+    persist_directory="/home/akash/HSv2"
+)
+
+retriever = vectorstore.as_retriever()
+
+
+prompt_template =("""
+<|user|>
+Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+{context}
+NTU is Nottingham Trent University.It is not Singapore. Note this it is very important.
+Question: {query}<|end|>
+<|assistant|>
+Helpful Answer:
+""")
+
+prompt=PromptTemplate(template=prompt_template,input_variables=["context","query"])
+
+llm = HuggingFacePipeline(pipeline=generate_text)
+
+chain = load_qa_chain(llm, chain_type="stuff")
+query = "Are there any connections with local employers?"
+doc = vectorstore.similarity_search(query)
+chain.run(input_documents = doc, question = query)
--- dataGenerator.py
+++ dataGenerator.py
@@ -1,25 +1,47 @@
-from langchain.document_loaders import DirectoryLoader
-loader = DirectoryLoader("your-directory")
-documents = loader.load()
+from ragas.testset.generator import TestsetGenerator  
+from ragas.testset.evolutions import simple, reasoning, multi_context  
+from langchain.text_splitter import RecursiveCharacterTextSplitter  
+from langchain.document_loaders import SeleniumURLLoader  
+from langchain_community.llms import Ollama  
+from langchain_community.embeddings import OllamaEmbeddings  
+
+urls = ["https://www.ntu.ac.uk/course/computer-science", 
+    "https://www.ntu.ac.uk/study-and-courses/courses/our-facilities/computer-science-facilities"]
+
+loader = SeleniumURLLoader(urls)  # Creating an instance of SeleniumURLLoader with the given URLs
+documents = loader.load()  # Loading the documents using the loader
 
 for document in documents:
-    document.metadata['filename'] = document.metadata['source']
+    document.metadata["filename"] = document.metadata["source"]  # Adding a "filename" key to the metadata dictionary of each document
 
-from ragas.testset.generator import TestsetGenerator
-from ragas.testset.evolutions import simple, reasoning, multi_context
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+# Split the documents into chunks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)  # Creating an instance of RecursiveCharacterTextSplitter with the given parameters
+all_splits = text_splitter.split_documents(documents)  # Splitting the documents into chunks using the text splitter
 
-# generator with openai models
-generator_llm = ChatOpenAI(model="gpt-3.5-turbo-16k")
-critic_llm = ChatOpenAI(model="gpt-4")
-embeddings = OpenAIEmbeddings()
+generator_llm = Ollama(model="phi3")  # Creating an instance of Ollama with the "phi3" model
+critic_llm = Ollama(model="llama3")  # Creating an instance of Ollama with the "llama3" model
+
+ollama_emb = OllamaEmbeddings(
+    model="nomic-embed-text",
+)  # Creating an instance of OllamaEmbeddings with the "nomic-embed-text" model
+
+print(generator_llm.invoke('Say hello'))  
+print(critic_llm.invoke('Say hello'))  
+
+r2 = ollama_emb.embed_query(
+    "What is the second letter of Greek alphabet"
+)  
+
+print(f'Embedding dimension: {len(r2)}')  
 
 generator = TestsetGenerator.from_langchain(
-    generator_llm,
-    critic_llm,
-    embeddings
-)
+    generator_llm=generator_llm,
+    critic_llm=critic_llm,
+    embeddings=ollama_emb
+) 
 
 # generate testset
-testset = generator.generate_with_langchain_docs(documents, test_size=10, distributions={simple: 0.5, reasoning: 0.25, multi_context: 0.25})
-testset.to_pandas()
+testset = generator.generate_with_langchain_docs(all_splits, test_size=10, distributions={simple: 0.5, reasoning: 0.25, multi_context: 0.25}, raise_exceptions=False)  # Generating a testset using the generator and the chunks of documents
+
+test_df = testset.to_pandas()  
+test_df.to_csv('testset.csv', index=False)  
