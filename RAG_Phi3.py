8ca186cfcef99a9ca9ca7c7fc51d32a1089c7a9e
Akash <akashkashyaps123@gmail.com>
2024-05-31 11:53:19 +0100

    wrote ragas dataset logic

--- RAG_Phi3.py
+++ RAG_Phi3.py
@@ -64,23 +64,23 @@ generate_text = transformers.pipeline(
     repetition_penalty=1.1
 )
 
-# Test the model with a prompt
-input_text = "what features does nottingham trent university computer science department have?"
-generated_ids = generate_text(input_text)[0]['generated_text']
-print(generated_ids)
+# # Test the model with a prompt
+# input_text = "what features does nottingham trent university computer science department have?"
+# generated_ids = generate_text(input_text)[0]['generated_text']
+# print(generated_ids)
 
 
-from langchain.document_loaders import SeleniumURLLoader
+# from langchain.document_loaders import SeleniumURLLoader
 
-urls = ["https://www.ntu.ac.uk/course/computer-science", 
-        "https://www.ntu.ac.uk/study-and-courses/academic-schools/science-and-technology/Computer-Science"]
+# urls = ["https://www.ntu.ac.uk/course/computer-science", 
+#         "https://www.ntu.ac.uk/study-and-courses/academic-schools/science-and-technology/Computer-Science"]
        
-loader = SeleniumURLLoader(urls)
-documents = loader.load()
+# loader = SeleniumURLLoader(urls)
+# documents = loader.load()
 
-# Split the documents into chunks
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
-all_splits = text_splitter.split_documents(documents)
+# # Split the documents into chunks
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+# all_splits = text_splitter.split_documents(documents)
 
 # Define the model name and kwargs for embeddings
 model_name = "sentence-transformers/all-mpnet-base-v2"
@@ -89,12 +89,14 @@ model_kwargs = {"device": "cuda"}
 # Create the HuggingFaceEmbeddings
 embeddings = HuggingFaceEmbeddings(model_name=model_name, model_kwargs=model_kwargs)
 
-# Create the vectorstore
-vectorstore = Chroma.from_documents(
-    all_splits,
-    embeddings,
-    persist_directory="/home/akash/HSv2"
-)
+# # Create the vectorstore
+# vectorstore = Chroma.from_documents(
+#     all_splits,
+#     embeddings,
+#     persist_directory="/home/akash/HSv2"
+# )
+
+vectorstore = Chroma(persist_directory='/home/akash/HSv2/', embedding_function=embeddings)
 
 retriever = vectorstore.as_retriever()
 
@@ -122,41 +124,226 @@ chain.run(input_documents = doc, question = query)
 from datasets import Dataset
 
 # Load test set
-testset4 = pd.read_csv('testset3.csv')
+test100 = pd.read_csv('100-phi3-llama3.csv')
 
 # Prepare dataset for batch processing
-dataset = Dataset.from_pandas(testset4)
+dataset = Dataset.from_pandas(test100)
 
-# Function to process each question
-def process_batch(batch):
-    questions = batch["question"]
-    contexts = []
-    answers = []
+from ragas.metrics import (
+    answer_relevancy,
+    faithfulness,
+    context_recall,
+    context_precision,
+    context_relevancy,
+    answer_correctness,
+    answer_similarity
+)
+
+from ragas.metrics.critique import harmfulness
+from ragas import evaluate
+
+def create_ragas_dataset(rag_pipeline, dataset):
+  rag_dataset = []
+  for row in tqdm(dataset):
+    answer = rag_pipeline.invoke({"question" : row["question"]})
+    rag_dataset.append(
+        {"question" : row["question"],
+         "answer" : answer["response"],
+         "contexts" : [context.page_content for context in answer["context"]],
+         "ground_truths" : [row["ground_truth"]]
+         }
+    )
+  rag_df = pd.DataFrame(rag_dataset)
+  rag_eval_dataset = Dataset.from_pandas(rag_df)
+  return rag_eval_dataset
+
+def evaluate_ragas_dataset(ragas_dataset):
+  result = evaluate(
+    ragas_dataset,
+    metrics=[
+        context_precision,
+        faithfulness,
+        answer_relevancy,
+        context_recall,
+        context_relevancy,
+        answer_correctness,
+        answer_similarity
+    ],
+  )
+  return result
+
+from tqdm import tqdm
+import pandas as pd
+
+qa_ragas_dataset = create_ragas_dataset((chain.run(input_documents = doc, question = "question"), dataset))
+qa_ragas_dataset[0]
+
+qa_result = evaluate_ragas_dataset(qa_ragas_dataset)
+qa_result[0]
+
+# # Function to process each question
+# def process_batch(batch):
+#     questions = batch["question"]
+#     contexts = []
+#     answers = []
     
-    for question in questions:
-        docs = vectorstore.similarity_search(question)
-        retrieved_context = " ".join([doc.page_content for doc in docs])
-        result = chain.run(input_documents=docs, question=question)
+#     for question in questions:
+#         docs = vectorstore.similarity_search(question)
+#         retrieved_context = " ".join([doc.page_content for doc in docs])
+#         result = chain.run(input_documents=docs, question=question)
         
-        if '\nHelpful Answer:' in result:
-            context, answer = result.split('\nHelpful Answer:', 1)
-            context = context.strip()
-            answer = answer.strip()
-        else:
-            context = retrieved_context
-            answer = "I don't know"
+#         if '\nHelpful Answer:' in result:
+#             context, answer = result.split('\nHelpful Answer:', 1)
+#             context = context.strip()
+#             answer = answer.strip()
+#         else:
+#             context = retrieved_context
+#             answer = "I don't know"
         
-        contexts.append(context)
-        answers.append(answer)
+#         contexts.append(context)
+#         answers.append(answer)
     
-    return {"RAG Context": contexts, "RAG Answer": answers}
+#     return {"RAG_context": contexts, "answer": answers}
 
-# Batch process the dataset
-results = dataset.map(process_batch, batched=True, batch_size=8)
+# # # Batch process the dataset
+# # results = dataset.map(process_batch, batched=True, batch_size=8)
+# import dill
+# # Serialize the process_batch function using dill
+# serialized_function = dill.dumps(process_batch)
 
-# Add the results to the DataFrame
-testset4['RAG Context'] = results['RAG Context']
-testset4['RAG Answer'] = results['RAG Answer']
+# # Define a wrapper function to deserialize and call the process_batch function
+# def process_batch_wrapper(batch):
+#     func = dill.loads(serialized_function)
+#     return func(batch)
 
-# Save the DataFrame with the results
-testset4.to_csv('testset3_with_results.csv', index=False)
+# # Batch process the dataset
+# results = dataset.map(process_batch_wrapper, batched=True, batch_size=8)
+
+# def generate_answer(question, vectorstore, chain):
+#     docs = vectorstore.similarity_search(question)
+#     retrieved_context = " ".join([doc.page_content for doc in docs])
+#     result = chain.run(input_documents=docs, question=question)
+
+#     if '\nHelpful Answer:' in result:
+#         context, answer = result.split('\nHelpful Answer:', 1)
+#         context = context.strip()
+#         answer = answer.strip()
+#     else:
+#         context = retrieved_context
+#         answer = "I don't know"
+
+#     return context, answer
+
+# def process_batch(batch, vectorstore, chain):
+#     questions = batch["question"]
+#     contexts = []
+#     answers = []
+
+#     for question in questions:
+#         context, answer = generate_answer(question, vectorstore, chain)
+#         contexts.append(context)
+#         answers.append(answer)
+
+#     return {"RAG_context": contexts, "answer": answers}
+
+# results = dataset.map(lambda batch: process_batch(batch, vectorstore, chain), batched=True, batch_size=8)
+
+
+# # Add the results to the DataFrame
+# test100['RAG_context'] = results['RAG_context']
+# test100['answer'] = results['answer']
+
+# # Save the DataFrame with the results
+# test100.to_csv('test100_with_results.csv', index=False)
+
+# from ragas.metrics import (
+#     answer_relevancy,
+#     faithfulness,
+#     context_recall,
+#     context_precision,
+#     context_relevancy,
+#     answer_correctness,
+#     answer_similarity
+# )
+
+# from ragas.metrics.critique import harmfulness
+# from ragas import evaluate
+# from tqdm import tqdm
+
+# def create_ragas_dataset(rag_pipeline, eval_dataset):
+#   rag_dataset = []
+#   for row in tqdm(eval_dataset):
+#     answer = rag_pipeline.invoke({"question" : row["question"]})
+#     rag_dataset.append(
+#         {"question" : row["question"],
+#          "answer" : answer["response"],
+#          "contexts" : [context.page_content for context in answer["context"]],
+#          "ground_truths" : [row["ground_truth"]]
+#          }
+#     )
+#   rag_df = pd.DataFrame(rag_dataset)
+#   rag_eval_dataset = Dataset.from_pandas(rag_df)
+#   return rag_eval_dataset
+
+# def evaluate_ragas_dataset(ragas_dataset):
+#   result = evaluate(
+#     ragas_dataset,
+#     metrics=[
+#         context_precision,
+#         faithfulness,
+#         answer_relevancy,
+#         context_recall,
+#         context_relevancy,
+#         answer_correctness,
+#         answer_similarity
+#     ],
+#   )
+#   return result
+
+# df = result.to_pandas()
+# df.head()
+
+# finaltest1 = pd.read_csv('testset3_with_results.csv')
+# # finaltest1.drop(columns=['contexts'], inplace=True)
+# finaltest1.rename(columns={'newAnswer': 'answer'}, inplace=True)
+# finaltest1
+
+
+# dataset = Dataset.from_pandas(finaltest1)
+# print(dataset.features["contexts"])
+
+# import ast
+
+# def preprocess_contexts(example):
+#     example["contexts"] = ast.literal_eval(example["contexts"])
+#     return example
+
+# dataset = dataset.map(preprocess_contexts)
+
+
+# score = evaluate(
+#     dataset,
+#     llm=llm,
+#     embeddings=embeddings,
+#     metrics=[
+#         context_precision,
+#         faithfulness,
+#         answer_relevancy,
+#         context_recall,
+#     ],
+# )
+
+# from ragas import evaluate
+
+# result = evaluate(
+#     finaltest1,
+#     metrics=[
+#         context_precision,
+#         faithfulness,
+#         answer_relevancy,
+#         context_recall,
+#     ],
+# )
+
+# result
+
