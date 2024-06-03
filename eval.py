a89b0edc9b6ca939ae9148a12dda8cefa7f774bf
Akash <akashkashyaps123@gmail.com>
2024-06-03 15:25:10 +0100

    created eval script

---
+++ eval.py
@@ -0,0 +1,126 @@
+# Import dependancies
+import torch
+import transformers
+import pandas as pd
+import numpy as np
+from torch import cuda, bfloat16
+from langchain.llms import HuggingFacePipeline
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain.chains import RetrievalQA
+from transformers import StoppingCriteriaList, StoppingCriteria
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from langchain.chains.question_answering import load_qa_chain
+from langchain import HuggingFaceHub
+from langchain.prompts import PromptTemplate
+
+# Define the model
+model_id = "microsoft/Phi-3-mini-4k-instruct"
+hf_auth = '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'
+tokenizer = transformers.AutoTokenizer.from_pretrained(
+    model_id,
+    use_auth_token=hf_auth,
+    trust_remote_code=True
+)
+device = f'cuda:{cuda.current_device()}' if cuda.is_available() else 'cpu'
+
+# Configure BitsAndBytesConfig for quantization
+bnb_config = transformers.BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type='nf4',
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=bfloat16
+)
+
+model_config = transformers.AutoConfig.from_pretrained(
+    model_id,
+    use_auth_token=hf_auth,
+    trust_remote_code=True
+)
+model = transformers.AutoModelForCausalLM.from_pretrained(
+    model_id,
+    trust_remote_code=True,
+    config=model_config,
+    quantization_config=bnb_config,
+    device_map='auto',
+    use_auth_token=hf_auth,
+)   
+
+# Set the model to evaluation mode
+model.eval()
+
+# Define the text generation pipeline
+generate_text = transformers.pipeline(
+    model=model,
+    tokenizer=tokenizer,
+    return_full_text=True,
+    task='text-generation',
+    max_new_tokens=512,
+    do_sample=True,
+    temperature=0.1,
+    repetition_penalty=1.1
+)
+
+# Define the model name and kwargs for embeddings
+model_name = "sentence-transformers/all-mpnet-base-v2"
+model_kwargs = {"device": "cuda"}
+
+# Create the HuggingFaceEmbeddings
+embeddings = HuggingFaceEmbeddings(model_name=model_name, model_kwargs=model_kwargs)
+
+vectorstore = Chroma(persist_directory='/home/akash/HSv2/HSv2/', embedding_function=embeddings)
+
+retriever = vectorstore.as_retriever(search_kwargs={'k': 1}, max_tokens=2500)
+
+
+prompt_template =("""
+<|user|>
+For the purposes of this conversation, you are a helpful agent who is present at a University Open Day at Nottingham Trent University (NTU) at Clifton Campus, Nottingham, United Kingdom. A University Open Day is an event where future University students will visit to see the campus, facilities, and meet the teaching staff. The future students are going to ask you questions about University which you will answer by using the helpful context at the end of this message. Note that it is very important that you are at Nottingham Trent University (NTU) in the United Kingdom and NOT Nanyang Technological University in Singapore. You will now be given context and asked a question. Your task is to answer the question. If you do not know the answer, just say that you cannot answer the question, do not try to make up an answer.
+CONTEXT: {context}
+QUESTION: {question}<|end|>
+<|assistant|>
+Helpful Answer:
+""")
+
+prompt=PromptTemplate(template=prompt_template,input_variables=["context","question"])
+
+llm = HuggingFacePipeline(pipeline=generate_text)
+
+chain = load_qa_chain(llm, chain_type="stuff", prompt=prompt)  
+
+from ragas.metrics import (
+    answer_relevancy,
+    faithfulness,
+    context_recall,
+    context_precision,
+    context_relevancy,
+    answer_correctness,
+    answer_similarity
+)
+from ragas.metrics.critique import harmfulness
+from ragas import evaluate
+
+def evaluate_ragas_dataset(ragas_dataset):
+  result = evaluate(
+    ragas_dataset,
+    llm=llm,
+    embeddings=embeddings,
+    metrics=[
+        context_precision,
+        faithfulness,
+        answer_relevancy,
+        context_recall,
+        context_relevancy,
+        answer_correctness,
+        answer_similarity
+    ],
+  )
+  return result
+
+evaluation_set = pd.read_csv("evaluation_set.csv")
+
+qa_result = evaluate_ragas_dataset(evaluation_set)
+qa_result.to_csv("qa_result.csv", index=False)
