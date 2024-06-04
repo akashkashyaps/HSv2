e3f1541515bebd24cc24f50a4f1e258df5f2ade2
Akash <akashkashyaps123@gmail.com>
2024-06-04 11:48:18 +0100

    using ollama for eval

--- eval.py
+++ eval.py
@@ -1,76 +1,17 @@
-# Import dependancies
+from langchain_community.llms import Ollama  
+from langchain_community.embeddings import OllamaEmbeddings
 import torch
-import transformers
-import pandas as pd
-import numpy as np
-from torch import cuda, bfloat16
-from langchain.llms import HuggingFacePipeline
-from langchain.document_loaders.csv_loader import CSVLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import Chroma
-from langchain_community.document_loaders.csv_loader import CSVLoader
-from langchain.chains import RetrievalQA
-from transformers import StoppingCriteriaList, StoppingCriteria
-from transformers import AutoTokenizer, AutoModelForCausalLM
-from langchain.chains.question_answering import load_qa_chain
-from langchain import HuggingFaceHub
-from langchain.prompts import PromptTemplate
+import pandas as pd 
 
-# Define the model
-model_id = "microsoft/Phi-3-mini-4k-instruct"
-hf_auth = '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'
-tokenizer = transformers.AutoTokenizer.from_pretrained(
-    model_id,
-    use_auth_token=hf_auth,
-    trust_remote_code=True
-)
-device = f'cuda:{cuda.current_device()}' if cuda.is_available() else 'cpu'
+# Check if CUDA is available
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'Using device: {device}')
 
-# Configure BitsAndBytesConfig for quantization
-bnb_config = transformers.BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_quant_type='nf4',
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_compute_dtype=bfloat16
-)
+llm = Ollama(model="llama3")  
 
-model_config = transformers.AutoConfig.from_pretrained(
-    model_id,
-    use_auth_token=hf_auth,
-    trust_remote_code=True
-)
-model = transformers.AutoModelForCausalLM.from_pretrained(
-    model_id,
-    trust_remote_code=True,
-    config=model_config,
-    quantization_config=bnb_config,
-    device_map='auto',
-    use_auth_token=hf_auth,
-)   
-
-# Set the model to evaluation mode
-model.eval()
-
-# Define the text generation pipeline
-generate_text = transformers.pipeline(
-    model=model,
-    tokenizer=tokenizer,
-    return_full_text=True,
-    task='text-generation',
-    max_new_tokens=512,
-    do_sample=True,
-    temperature=0.1,
-    repetition_penalty=1.1
-)
-
-# Define the model name and kwargs for embeddings
-model_name = "sentence-transformers/all-mpnet-base-v2"
-model_kwargs = {"device": "cuda"}
-
-# Create the HuggingFaceEmbeddings
-embeddings = HuggingFaceEmbeddings(model_name=model_name, model_kwargs=model_kwargs)
-llm = HuggingFacePipeline(pipeline=generate_text)
+ollama_emb = OllamaEmbeddings(
+    model="nomic-embed-text",
+)  
 
 
 from ragas.metrics import (
@@ -90,7 +31,7 @@ def evaluate_ragas_dataset(ragas_dataset):
   result = evaluate(
     ragas_dataset,
     llm=llm,
-    embeddings=embeddings,
+    embeddings=ollama_emb,
     metrics=[
         context_precision,
         faithfulness,
@@ -103,26 +44,54 @@ def evaluate_ragas_dataset(ragas_dataset):
   )
   return result
 
-import ast  # Import the ast module to safely evaluate string representations of lists
+# import ast  # Import the ast module to safely evaluate string representations of lists
 
-def preprocess_data(data):
-    preprocessed_data = []
-    for _, row in data.iterrows():
-        contexts = ast.literal_eval(row["contexts"])  # Convert the string representation to a list
-        preprocessed_data.append({
-            "question": row["question"],
-            "context": row["context"],
-            "ground_truth": row["answer"],
-            "contexts": contexts  # Use the converted list as the "contexts" field
-        })
-    return preprocessed_data
+# def preprocess_data(data):
+#     preprocessed_data = []
+#     for _, row in data.iterrows():
+#         contexts = ast.literal_eval(row["contexts"])  # Convert the string representation to a list
+#         preprocessed_data.append({
+#             "question": row["question"],
+#             "context": row["context"],
+#             "ground_truth": row["answer"],
+#             "contexts": contexts  # Use the converted list as the "contexts" field
+#         })
+#     return preprocessed_data
 
 evaluation_set = pd.read_csv("evaluation_set.csv")
-preprocessed_data = preprocess_data(evaluation_set)
+# Convert the context column to a list of strings
+evaluation_set['context'] = evaluation_set['context'].apply(lambda x: [x])
+evaluation_set
 
-qa_result = evaluate_ragas_dataset(preprocessed_data)
+# preprocessed_data = preprocess_data(evaluation_set)
+
+from datasets import Dataset
+
+data = [
+    {
+        "question": row["question"],
+        "context": row["context"],
+        "ground_truth": row["answer"],
+        "contexts": row["contexts"].split("\n") 
+    }
+    for _, row in evaluation_set.iterrows()
+]
+
+dataset = Dataset.from_dict(data)
+
+
+qa_result = evaluate_ragas_dataset(data)
 qa_result.to_csv("qa_result.csv", index=False)
 
 
 
 
+# from datasets import load_dataset
+# eng = load_dataset("explodinggradients/amnesty_qa","english")
+# eng["eval"].to_csv("eng.csv", index=False)
+
+
+# import pandas as pd
+# eng_data = pd.read_csv("eng.csv")
+
+# eng_data.head(1)["contexts"].values[0]
