51201621a370b466c73d624147103bbf660c50c8
Akash <akashkashyaps123@gmail.com>
2024-06-04 14:42:02 +0100

    qualitative mterics

--- eval.py
+++ eval.py
@@ -17,96 +17,68 @@ ollama_emb = OllamaEmbeddings(
 )  
 
 
-from ragas.metrics import (
-    answer_relevancy,
-    faithfulness,
-    context_recall,
-    context_precision,
-    context_relevancy,
-    answer_correctness,
-    answer_similarity
-)
+# from ragas.metrics import (
+#     answer_relevancy,
+#     faithfulness,
+#     context_recall,
+#     context_precision,
+#     context_relevancy,
+#     answer_correctness,
+#     answer_similarity
+# )
 from ragas.metrics.critique import harmfulness
+from ragas.metrics.critique import maliciousness
+from ragas.metrics.critique import coherence
+from ragas.metrics.critique import conciseness
 from ragas import evaluate
 from datasets import Dataset
 
-def evaluate_ragas_dataset(ragas_dataset):
+# def evaluate_ragas_dataset(ragas_dataset):
+#   result = evaluate(
+#     ragas_dataset,
+#     llm=llm,
+#     embeddings=ollama_emb,
+#     metrics=[
+#         context_precision,
+#         faithfulness,
+#         answer_relevancy,
+#         context_recall,
+#         context_relevancy,
+#         answer_correctness,
+#         answer_similarity
+#     ],
+#   )
+#   return result
+
+
+def qualitative_analysis(ragas_dataset):
   result = evaluate(
     ragas_dataset,
     llm=llm,
     embeddings=ollama_emb,
     metrics=[
-        context_precision,
-        faithfulness,
-        answer_relevancy,
-        context_recall,
-        context_relevancy,
-        answer_correctness,
-        answer_similarity
+        harmfulness,
+        maliciousness,
+        coherence,
+        conciseness
     ],
   )
   return result
 
 
 
-# import ast  # Import the ast module to safely evaluate string representations of lists
-
-# def preprocess_data(data):
-#     preprocessed_data = []
-#     for _, row in data.iterrows():
-#         contexts = ast.literal_eval(row["contexts"])  # Convert the string representation to a list
-#         preprocessed_data.append({
-#             "question": row["question"],
-#             "context": row["context"],
-#             "ground_truth": row["answer"],
-#             "contexts": contexts  # Use the converted list as the "contexts" field
-#         })
-#     return preprocessed_data
-
 evaluation_set = pd.read_csv("evaluation_set.csv")
+evaluation_set = evaluation_set.head(5)
 # Convert the context column to a list of strings
 evaluation_set['context'] = evaluation_set['context'].apply(lambda x: [x])
 evaluation_set.drop(columns=["contexts"], inplace=True)
 evaluation_set.rename(columns={"context": "contexts"}, inplace=True)
 
-# preprocessed_data = preprocess_data(evaluation_set)
 
 from datasets import Dataset
-
-# data = [
-#     {
-#         "question": row["question"],
-#         "context": row["context"],
-#         "ground_truth": row["answer"],
-#         "contexts": row["contexts"].split("\n") 
-#     }
-#     for _, row in evaluation_set.iterrows()
-# ]
-
 dataset = Dataset.from_pandas(evaluation_set)
 
-# dataset
-# def custom_evaluate_ragas_dataset(evaluation_set):
-#     def custom_remap_column_names(dataset, column_map):
-#         inverse_column_map = {v: k for k, v in column_map.items()}
-#         return dataset.rename(columns=inverse_column_map)
-
-#     qa_result = evaluate_ragas_dataset(evaluation_set, custom_remap_column_names)
-#     return qa_result
-
-
-qa_result = evaluate_ragas_dataset(dataset)
-qa_result.to_pandas().to_csv("qa_result.csv", index=False)
-
-
-
-
-# from datasets import load_dataset
-# eng = load_dataset("explodinggradients/amnesty_qa","english")
-# eng["eval"].to_csv("eng.csv", index=False)
-
-
-# import pandas as pd
-# eng_data = pd.read_csv("eng.csv")
-
-# eng_data.head(1)["contexts"].values[0]
+# qa_result = evaluate_ragas_dataset(dataset)
+qualitative_result = qualitative_analysis(dataset)
+# qa_result.to_pandas().to_csv("qa_result.csv", index=False)
+qualitative_result.to_pandas().to_csv("qualitative_result.csv", index=False)
