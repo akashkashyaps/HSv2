3aab379c87beaafef4d5cc4c8adfd99ca403c8b5
Akash <akashkashyaps123@gmail.com>
2024-06-05 16:13:19 +0100

    llama eval

--- eval.py
+++ eval.py
@@ -10,7 +10,7 @@ nest_asyncio.apply()
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f'Using device: {device}')
 
-llm = Ollama(model="mistral")  
+llm = Ollama(model="gemma")  
 
 ollama_emb = OllamaEmbeddings(
     model="nomic-embed-text",
@@ -81,6 +81,6 @@ from datasets import Dataset
 dataset = Dataset.from_pandas(evaluation_set)
 
 # quantitative_result_qwen = evaluate_ragas_dataset(dataset)
-qualitative_result_mistral = qualitative_analysis(dataset)
+qualitative_result_gemma = qualitative_analysis(dataset)
 # quantitative_result_qwen.to_pandas().to_csv("quantitative_result_qwen.csv", index=False)
-qualitative_result_mistral.to_pandas().to_csv("base_LLaMa3-Evaluator_Mistral7B-qualitative.csv", index=False)
+qualitative_result_gemma.to_pandas().to_csv("base_LLaMa3-Evaluator_Gemma-qualitative.csv", index=False)
