df42ec0cb421c127b391016a91846b44ae6af2cd
Akash <akashkashyaps123@gmail.com>
2024-06-06 11:41:25 +0100

    eval mistral

--- eval.py
+++ eval.py
@@ -10,7 +10,7 @@ nest_asyncio.apply()
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f'Using device: {device}')
 
-llm = Ollama(model="llama3")  
+llm = Ollama(model="mistral")  
 
 ollama_emb = OllamaEmbeddings(
     model="nomic-embed-text",
@@ -43,7 +43,7 @@ from datasets import Dataset
 #         context_precision,
 #         faithfulness,
 #         answer_relevancy,
-#         context_recall,
+#         context_recall,Phi-3: 
 #         context_relevancy,
 #         answer_correctness,
 #         answer_similarity
@@ -81,6 +81,6 @@ from datasets import Dataset
 dataset = Dataset.from_pandas(evaluation_set)
 
 # quantitative_result_qwen = evaluate_ragas_dataset(dataset)
-qualitative_result_llama = qualitative_analysis(dataset)
+qualitative_result_mistral = qualitative_analysis(dataset)
 # quantitative_result_qwen.to_pandas().to_csv("quantitative_result_qwen.csv", index=False)
-qualitative_result_llama.to_pandas().to_csv("Base_Mistral7B-Evaluator_LLaMa3-qualitative.csv", index=False)
+qualitative_result_mistral.to_pandas().to_csv("Base_Mistral7B-Evaluator_Mistral7B-qualitative.csv", index=False)
