f9ff5df56af04e46f6e3fec059b81c31e5ef8608
Akash <akashkashyaps123@gmail.com>
2024-07-23 14:50:32 +0100

    added test set code

--- AdvancedRAG_Mistral.py
+++ AdvancedRAG_Mistral.py
@@ -144,7 +144,7 @@ from tqdm import tqdm
 import pandas as pd
 
 # Create test set
-testVanilla = pd.read_csv('ROBIN_FINAL_TEST_SET.csv')
+testVanilla = pd.read_excel('ROBIN_FINAL_TEST_SET.xlsx')
 questions = testVanilla['question'].tolist()
 
 # Create empty lists to store the results and the time taken
