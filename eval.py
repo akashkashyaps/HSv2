0475a9e9238edf6c117a6b2b9821a8e2f1352dae
Akash <akashkashyaps123@gmail.com>
2024-07-31 14:37:25 +0100

    eval fusion

--- eval.py
+++ eval.py
@@ -10,7 +10,7 @@ nest_asyncio.apply()
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f'Using device: {device}')
 
-llm = Ollama(model="mistral")  
+llm = Ollama(model="internlm2")  
 
 ollama_emb = OllamaEmbeddings(
     model="nomic-embed-text",
@@ -86,5 +86,5 @@ dataset = Dataset.from_pandas(evaluation_set)
 
 quantitative_result_qwen = evaluate_ragas_dataset(dataset)
 # qualitative_result_qwen = qualitative_analysis(dataset)
-quantitative_result_qwen.to_pandas().to_csv("Base_Fusion-Evaluator_mistral-quantitative.csv", index=False)
+quantitative_result_qwen.to_pandas().to_csv("Base_Fusion-Evaluator_internlm2-quantitative.csv", index=False)
 # qualitative_result_qwen.to_pandas().to_csv("Base_Mistral7B-Evaluator_Qwen-qualitative.csv", index=False)
