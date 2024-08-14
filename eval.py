dc4841f9fe9b2b9bea3ac7ec0e4466e0e902531a
Akash <akashkashyaps123@gmail.com>
2024-08-14 12:35:31 +0100

    trying to fill nan

--- eval.py
+++ eval.py
@@ -10,7 +10,7 @@ nest_asyncio.apply()
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f'Using device: {device}')
 
-llm = Ollama(model="mistral")  
+llm = Ollama(model="internlm2")  
 
 ollama_emb = OllamaEmbeddings(
     model="nomic-embed-text",
@@ -20,8 +20,8 @@ ollama_emb = OllamaEmbeddings(
 from ragas.metrics import (
     # answer_relevancy,
     # faithfulness,
-    context_recall,
-    context_precision,
+    # context_recall,
+    # context_precision,
     # context_relevancy,
     # answer_correctness,
     # answer_similarity,
@@ -42,10 +42,10 @@ def evaluate_ragas_dataset(ragas_dataset):
     embeddings=ollama_emb,
     raise_exceptions=False,
     metrics=[
-        context_precision,
+        # context_precision,
         # faithfulness,
         # answer_relevancy,
-        context_recall, 
+        # context_recall, 
         # context_relevancy,
         # answer_correctness,
         # answer_similarity,
@@ -73,7 +73,7 @@ def evaluate_ragas_dataset(ragas_dataset):
 
 
 
-evaluation_set = pd.read_csv("evaluation_set_Advanced.csv")
+evaluation_set = pd.read_csv("vanilla_internlm2_nan1.csv")
 # evaluation_set = evaluation_set.head(5)
 # Convert the context column to a list of strings
 evaluation_set['context'] = evaluation_set['context'].apply(lambda x: [x])
@@ -86,5 +86,5 @@ dataset = Dataset.from_pandas(evaluation_set)
 
 quantitative_result_qwen = evaluate_ragas_dataset(dataset)
 # qualitative_result_qwen = qualitative_analysis(dataset)
-quantitative_result_qwen.to_pandas().to_csv("Base_Advanced-Evaluator_mistral-quantitative.csv", index=False)
+quantitative_result_qwen.to_pandas().to_csv("Base_Vanilla-Evaluator_internlm2_v2-quantitative.csv", index=False)
 # qualitative_result_qwen.to_pandas().to_csv("Base_Mistral7B-Evaluator_Qwen-qualitative.csv", index=False)
