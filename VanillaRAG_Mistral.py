0a0a3fe7d8a0d9a1f71d4257649f0b965e54a92a
Akash <akashkashyaps123@gmail.com>
2024-08-23 12:25:57 +0100

    changing chain to add new way of memory

--- VanillaRAG_Mistral.py
+++ VanillaRAG_Mistral.py
@@ -221,7 +221,7 @@ class ChatHistoryManager:
 history_manager = ChatHistoryManager()
 
 from langchain.chains import LLMChain
-rag_chain = LLMChain(llm=llm, prompt=prompt_template)
+rag_chain = LLMChain(llm=llm, prompt=prompt)
 
 from llm_guard.input_scanners import PromptInjection, BanTopics, Toxicity as InputToxicity
 from llm_guard.input_scanners.prompt_injection import MatchType as InputMatchType
