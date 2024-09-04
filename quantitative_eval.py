ac521dcac7421dd504a9592987db918e9e277e47
Akash <akashkashyaps123@gmail.com>
2024-09-04 12:35:40 +0100

    CREATED QUANT AND QUAL EVAL SCRIPTS

--- quantitative_eval.py
+++ quantitative_eval.py
@@ -10,7 +10,7 @@ nest_asyncio.apply()
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f'Using device: {device}')
 
-llm = Ollama(model="qwen2")  
+llm = Ollama(model="llama3")  
 
 ollama_emb = OllamaEmbeddings(
     model="nomic-embed-text",
@@ -76,5 +76,5 @@ dataset = Dataset.from_pandas(evaluation_set)
 
 quantitative_result_qwen = evaluate_ragas_dataset(dataset)
 # qualitative_result_qwen = qualitative_analysis(dataset)
-quantitative_result_qwen.to_pandas().to_csv("Base_FusionMistral-Evaluator_qwen2-quantitative.csv", index=False)
+quantitative_result_qwen.to_pandas().to_csv("Base_FusionMistral-Evaluator_llama3-quantitative.csv", index=False)
 # qualitative_result_qwen.to_pandas().to_csv("Base_Mistral7B-Evaluator_Qwen-qualitative.csv", index=False)
