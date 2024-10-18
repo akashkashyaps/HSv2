f5be5ef6e1c1309e043f653e56ede81eaa3c968c
Akash <akashkashyaps123@gmail.com>
2024-10-18 13:45:00 +0100

    fix:vectorstore

--- RAG_TTS_Ollama_Edge.py
+++ RAG_TTS_Ollama_Edge.py
@@ -72,12 +72,7 @@ import os
 
 home_directory = os.path.expanduser("~")
 persist_directory = os.path.join(home_directory, "HSv2", "vecdb")
-vectorstore = Chroma.from_documents(
-    recreated_splits,
-    embeddings,
-    collection_name="ROBIN-2",
-    persist_directory=persist_directory
-)
+vectorstore = Chroma(persist_directory=persist_directory, embedding_function=embeddings, collection_name="ROBIN-2")
 
 retriever_vanilla = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 2})
 retriever_mmr = vectorstore.as_retriever(search_type="mmr", search_kwargs={"k": 2})
