470dd245ad273ded20e674418e6c98e8bb1acd24
Akash <akashkashyaps123@gmail.com>
2024-10-21 15:46:25 +0100

    same

--- RAG_Groq.py
+++ RAG_Groq.py
@@ -81,11 +81,7 @@ import os
 
 home_directory = os.path.expanduser("~")
 persist_directory = os.path.join(home_directory, "HSv2", "vecdb")
-vectorstore = Chroma(
-    embeddings,
-    collection_name="ROBIN-3",
-    persist_directory=persist_directory
-)
+vectorstore = Chroma(persist_directory=persist_directory, embedding_function=embeddings, collection_name="ROBIN-3")
 
 retriever_vanilla = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 2})
 retriever_mmr = vectorstore.as_retriever(search_type="mmr", search_kwargs={"k": 2})
