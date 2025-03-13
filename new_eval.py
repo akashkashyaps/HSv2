529cd52d316e67885231478e65f6b73b49b74e55
Akash <akashkashyaps123@gmail.com>
2025-03-13 15:24:52 +0000

    new model

--- new_eval.py
+++ new_eval.py
@@ -283,16 +283,16 @@ You are FORBIDDEN from explaining your value.
 
 # Configuration
 MODELS = [
-    "llama3.1:8b-instruct-q4_0"
+    "deepseek-r1:8b-llama-distill-q4_K_M"
 ]
 #   "qwen2.5:7b-instruct-q4_0"
 #   "gemma2:9b-instruct-q4_0",
 #   "phi3.5:3.8b-mini-instruct-q4_0",
 #   "mistral:7b-instruct-q4_0",
-#   "llama3.1:8b-instruct-q4_0",
+#"llama3.1:8b-instruct-q4_0",
 #"lly/InternLM3-8B-Instruct:8b-instruct-q4_0"
 #"deepseek-r1:7b-qwen-distill-q4_K_M",
-#"deepseek-r1:8b-llama-distill-q4_K_M"
+#   "deepseek-r1:8b-llama-distill-q4_K_M"
 
 CSV_FILES = [
     "Results_lly_InternLM3-8B-Instruct_8b-instruct-q4_0.csv"
